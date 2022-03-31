02c6d290ae2d7e0bf3fcdb02af39c2294578f7b7
JongWoo Han <jongwoo0221@gmail.com>
2022-04-01 00:53:54 +0900

    chore: Setup initial tsc settings

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typescipt로 블록체인 만들기
+---
+
+Typescipt에 대해 간단히 공부하고자 노마드 코더의 강의롤 보며 진행하는 clone coding 입니다.
+
+### 참고
+[Nomadcoders - Typescript로 블록체인 만들기](https://nomadcoders.co/typescript-for-beginners) 
---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'Jongwoo', age = 24, gender = 'male';
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
